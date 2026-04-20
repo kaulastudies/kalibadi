76ed3d51f7db82d2b487463b6a5a2dd8f529de02
kaulastudies <kaulastudies@gmail.com>
2026-04-20 12:13:57 +0530

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+// supabase-config.js
+const SB_URL = "https://your-project-id.supabase.co"; // Replace with your actual Supabase URL
+const SB_KEY = "your-anon-key"; // Replace with your actual Supabase Anon Key
+const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
