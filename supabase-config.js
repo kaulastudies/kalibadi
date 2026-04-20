4bb849a6a1b63261f97b57edd35abbcfae6464ce
kaulastudies <kaulastudies@gmail.com>
2026-04-20 13:00:57 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,5 @@
 // supabase-config.js
-const SB_URL = "https://your-project-id.supabase.co"; // Replace with your actual Supabase URL
-const SB_KEY = "your-anon-key"; // Replace with your actual Supabase Anon Key
+const SB_URL = "https://cogoohothrzpndgszssl.supabase.co"; 
+const SB_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvZ29vaG90aHJ6cG5kZ3N6c3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NjU2MjYsImV4cCI6MjA5MjI0MTYyNn0.z2Ur5POtN3IRjV1DANGwmMTnPaJMD8SVCYeg557-k_Y"; 
+
 const supabaseClient = supabase.createClient(SB_URL, SB_KEY);
